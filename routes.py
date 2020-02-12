ccdf0ef62f4263c6c99da1b8b1a757b0fad322c8
Lizzzcai <lilcolinn@gmail.com>
2020-02-13 01:23:08 +0800

    update routes

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import url_for, render_template, redirect
+from flask import url_for, render_template, redirect, flash
 from flask import current_app as app
 from forms import ContactForm, SignupForm, TemperatureSubmissionForm
 
@@ -11,16 +11,27 @@ def home():
 @app.route('/temperature', methods=['GET','POST'])
 def temperature():
     form = TemperatureSubmissionForm()
-    first = True
+
+
+    print(form.errors)
+    if request.method == 'POST':
+        emp_id=request.form['emp_id']
+        emp_name=request.form['emp_id']
+        temperature=request.form['temperature']
+        print(f'{emp_id}, {emp_name}, {temperature}')
+
+    if form.validate():
+    # Save the comment here.
+        flash('Thanks for submission ' + emp_id)
+    else:
+        flash('Error: All the form fields are required. ')
+
     if form.validate_on_submit():
         print("form.validate_on_submit():", True)
         return redirect(url_for('success'), code=200)
-        #return redirect('/success')
     else:
         print("form.validate_on_submit():", False)
-        if not first:
-            return redirect(url_for('success'), code=200)
-    first = False
+
     return render_template('temperature.html',
                            form=form,
                            template='form-template')
